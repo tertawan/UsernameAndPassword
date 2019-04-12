23dea5ffa3f6a43872cc3d6b97d059256d680496
Ninx <xasnaxxasnax@gmail.com>
2019-04-12 09:21:17 +0700

    Initial Commit

---
+++ UserInterface/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UserInterface
+//
+//  Created by Ninxi on 12/4/2562 BE.
+//  Copyright © 2562 Ninxxi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
